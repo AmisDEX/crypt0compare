7a1de68b702cedf8ade161b17de4b2fe11cb476c
amisolution <siriusgem1@gmail.com>
2018-08-16 17:39:18 +0000

    Update ccc-streamer-utilities.js

--- streamer/ccc-streamer-utilities.js
+++ streamer/ccc-streamer-utilities.js
@@ -47,7 +47,8 @@ CCC.STATIC.CURRENCY.getSymbol = function(symbol) {
 CCC.STATIC.UTIL = {
   exchangeNameMapping: {
     'CCCAGG': 'CryptoCompare Index',
-    'BTCChina': 'BTCC'
+    'BTCChina': 'BTCC',
+    'EtherDelta': 'ETHERDELTA'
   },
   isMobile: function(userAgent) {
     if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(userAgent) ||
